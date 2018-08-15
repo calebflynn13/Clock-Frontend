059f41386a04ad97baf18cadb05ceb618c59cf70
calebflynn13 <calebflynn13@gmail.com>
2018-08-15 18:29:17 -0400

    update to segments of 4 LEDs

--- src/UpdateColors.java
+++ src/UpdateColors.java
@@ -73,39 +73,27 @@ public class UpdateColors extends Thread{
                 String ledCommand = "2:";
                 // send first 2 numbers
                 for (int i = 0; i < 9; i++) {
-                    // ledCommand += ((i * 3) + ",");
                     ledCommand += (clock.segments.get(i).toString());
-
-                    // ledCommand += ((i * 3 + 1) + ",");
                     ledCommand += (clock.segments.get(i).toString());
-
-                    // ledCommand += ((i * 3 + 2) + ",");
+                    ledCommand += (clock.segments.get(i).toString());
                     ledCommand += (clock.segments.get(i).toString());
                 }
 
                 // send colon
-                // ledCommand += ((27) + ","); // LED #30
                 ledCommand += (clock.segments.get(9).toString());
-
-                // ledCommand += ((28) + ","); //LED #31
                 ledCommand += (clock.segments.get(10).toString());
 
 
                 //last 2 numbers
                 for (int i = 11; i < 25; i++) {
-                    // ledCommand += ((i * 3 - 4) + ",");
                     ledCommand += (clock.segments.get(i).toString());
-
-                    // ledCommand += ((i * 3 - 3) + ",");
                     ledCommand += (clock.segments.get(i).toString());
-
-                    // ledCommand += ((i * 3 - 2) + ",");
+                    ledCommand += (clock.segments.get(i).toString());
                     ledCommand += (clock.segments.get(i).toString());
                 }
 
                 // AM/PM and reverse order Sat - Sun
                 for (int i = 25; i < 27; i++) {
-                    // ledCommand += ((46 + i) + ","); //LED #74 - 82
                     ledCommand += (clock.segments.get(i).toString());
                 }
                 System.out.println(ledCommand);
